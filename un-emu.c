24eed088a2492f24ee6ea20a9d3cead5f8520f34
Segher Boessenkool <segher@kernel.crashing.org>
2008-09-03 08:20:51 +0200

    Implement some insns

--- un-emu.c
+++ un-emu.c
@@ -9,11 +9,19 @@
 #include "types.h"
 #include "disas.h"
 
+
 #define N_MEM 0x400000
 
-static u16 mem[N_MEM];
 
+static u16 mem[N_MEM];
 static u16 reg[8];
+static u64 insn_count = 0;
+
+
+static u32 cs_pc(void)
+{
+	return ((reg[6] & 0x3f) << 16) | reg[7];
+}
 
 static void print_state(void)
 {
@@ -27,11 +35,112 @@ static void print_state(void)
 	printf(" %x%x%x%x", (reg[6] >> 6) & 1, (reg[6] >> 7) & 1,
 	                    (reg[6] >> 8) & 1, (reg[6] >> 9) & 1);
 	printf(" %02x\n", reg[6] >> 10);
-	disas(mem, ((reg[6] & 0x3f) << 16) | reg[7]);
+	disas(mem, cs_pc());
 }
 
 static void step(void)
 {
+	u16 op;
+	u8 op0, opA, op1, opN, opB, opimm;
+	u16 x, x0, x1;
+	u32 old_cs_pc = cs_pc();
+	u32 d;
+
+	op = mem[cs_pc()];
+	reg[7]++;
+
+
+	// the top four bits are the alu op or the branch condition, or E or F
+	op0 = (op >> 12);
+
+	// the next three are usually the destination register
+	opA = (op >> 9) & 7;
+
+	// and the next three the addressing mode
+	op1 = (op >> 6) & 7;
+
+	// the next three can be anything
+	opN = (op >> 3) & 7;
+
+	// and the last three usually the second register (source register)
+	opB = op & 7;
+
+	// the last six sometimes are a single immediate number
+	opimm = op & 63;
+
+
+	// jumps
+	if (opA == 7 && op1 < 2) {
+		switch (op0) {
+		case 14:
+			goto do_jump;
+		default:
+			goto bad;
+
+		do_jump:
+			if (op1 == 0)
+				reg[7] += opimm;
+			else
+				reg[7] -= opimm;
+			return;
+		}
+	}
+
+
+	// first, get the arguments
+	x0 = reg[opA];
+
+	switch (op1) {
+	case 3:
+		if ((opN & 3) == 3)
+			reg[opB]++;
+		d = reg[opB];
+		if (opN & 4)
+			d |= (reg[6] << 6) & 0x3f0000;
+		x1 = mem[d];
+		if ((opN & 3) == 1)
+			reg[opB]--;
+		if ((opN & 3) == 2)
+			reg[opB]++;
+		break;
+	case 4:
+		switch(opN) {
+		case 1:
+			x0 = reg[opB];
+			x1 = mem[cs_pc()];
+			reg[7]++;
+			break;
+		default:
+			goto bad;
+		}
+		break;
+	default:
+		goto bad;
+	}
+
+printf("--> args: %04x %04x\n", x0, x1);
+
+	// then, perform the alu op
+	switch (op0) {
+	case 9:
+		x = x1;
+		// XXX: flags
+		break;
+	default:
+		goto bad;
+	}
+
+printf("--> result: %04x\n", x);
+
+	reg[opA] = x;
+	return;
+
+
+bad:
+	printf("\n[insn_count = %llu]\n", insn_count);
+	printf("UNIMPLEMENTED:\n");
+	disas(mem, old_cs_pc);
+	exit(1);
 }
 
 int main(void)
@@ -49,6 +158,7 @@ int main(void)
 	for (;;) {
 		print_state();
 		step();
+		insn_count++;
 	}
 
 	return 0;
