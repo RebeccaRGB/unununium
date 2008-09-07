675e63b4d3b16e142abb23a73c183d5a79c7fc20
Segher Boessenkool <segher@kernel.crashing.org>
2008-09-07 12:02:16 +0200

    Moving pictures
    Nevermind the colours...

--- sdl.c
+++ sdl.c
@@ -53,5 +53,71 @@ void sdl_init(void)
 
 void update_screen(u16 *mem)
 {
-	printf("LOOK, NO HANDS!\n");
+	printf("\n\n");
+	printf("-----  VIDEO UPDATE  -----\n");
+
+	printf("page 0:\n");
+	printf("  dunno0 = %04x\n", mem[0x2810]);
+	printf("  dunno1 = %04x\n", mem[0x2811]);
+	printf("  mode   = %04x\n", mem[0x2812]);
+	printf("  flags  = %04x\n", mem[0x2813]);
+	printf("  tiles  = %04x\n", mem[0x2814]);
+	printf("  dunno5 = %04x\n", mem[0x2815]);
+	printf("  bitmap = %04x\n", mem[0x2820]);
+
+	printf("page 1:\n");
+	printf("  dunno0 = %04x\n", mem[0x2816]);
+	printf("  dunno1 = %04x\n", mem[0x2817]);
+	printf("  mode   = %04x\n", mem[0x2818]);
+	printf("  flags  = %04x\n", mem[0x2819]);
+	printf("  tiles  = %04x\n", mem[0x281a]);
+	printf("  dunno5 = %04x\n", mem[0x281b]);
+	printf("  bitmap = %04x\n", mem[0x2821]);
+
+	printf("sprites:\n");
+	printf("  bitmap = %04x\n", mem[0x2822]);
+
+	printf("\n");
+
+	if (SDL_MUSTLOCK(screen))
+		if (SDL_LockSurface(screen) < 0) {
+			printf("oh crap.\n");
+			exit(1);
+		}
+
+	u32 x0, y0, x1, y1;
+	u32 bitmap = 0x40*mem[0x2820];
+	u32 tilemap = mem[0x2814];
+
+	for (y0 = 0; y0 < 15; y0++)
+		for (x0 = 0; x0 < 20; x0++) {
+			u16 tile = mem[tilemap + x0 + 32*y0];
+
+			if (tile == 0)
+				continue;
+
+			for (y1 = 0; y1 < 16; y1++)
+				for (x1 = 0; x1 < 16; x1++) {
+					u16 c;
+
+					c = mem[bitmap + 128*tile + (16*y1 + x1)/2];
+					if (x1 & 1)
+						c >>= 8;
+					else
+						c &= 0xff;
+
+					c = mem[0x2b00 + c];
+
+					if (c & 0x8000)
+						continue;
+
+					u16 *buf = (u16 *)screen->pixels + (16*y0 + y1)*screen->pitch/2 + (16*x0 + x1);
+					*buf = c;
+				}
+		}
+
+	if (SDL_MUSTLOCK(screen))
+		SDL_UnlockSurface(screen);
+
+	SDL_UpdateRect(screen, 0, 0, 320, 240);
 }
