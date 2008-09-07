dcb59c6b1e2529f78eb049da6c95cc3565f17a0a
Segher Boessenkool <segher@kernel.crashing.org>
2008-09-07 21:20:13 +0200

    Refactoring and cleanup.

--- sdl.c
+++ sdl.c
@@ -5,52 +5,10 @@
 #include "sdl.h"
 
 
-static SDL_Surface *screen;
+static SDL_Surface *surface;
+static u16 *screen;
+static u32 pitch;
 
-
-static void ttt(int x, int y)
-{
-	u32 color = SDL_MapRGB(screen->format, 250, 150, 100);
-
-	u16 *bufp;
-
-	bufp = (u16 *)screen->pixels + y*screen->pitch/2 + x;
-	*bufp = color;
-}
-
-void sdl_init(void)
-{
-	if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0) {
-		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
-		exit(1);
-	}
-	atexit(SDL_Quit);
-
-	screen = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
-	if (!screen) {
-		fprintf(stderr, "Unable to set 320x240 video: %s\n", SDL_GetError());
-		exit(1);
-	}
-
-	{
-		int x, y;
-
-		if (SDL_MUSTLOCK(screen))
-			if (SDL_LockSurface(screen) < 0) {
-				printf("oh crap.\n");
-				exit(1);
-			}
-
-		for (y = 0; y < 240; y++)
-			for (x = 0; x < 320; x++)
-				ttt(x, y);
-
-		if (SDL_MUSTLOCK(screen))
-			SDL_UnlockSurface(screen);
-
-		SDL_UpdateRect(screen, 0, 0, 320, 240);
-	}
-}
 
 static u8 x58(u32 x)
 {
@@ -61,25 +19,29 @@ static u8 x58(u32 x)
 
 static void blit_16x16x256(u16 *dest, u16 *mem, u32 bitmap, u16 tile, u32 *palette)
 {
-	u32 x1, y1;
+	u32 x, y;
+	u16 *m = mem + bitmap + 128*tile;
 
-	for (y1 = 0; y1 < 16; y1++)
-		for (x1 = 0; x1 < 16; x1++) {
+	for (y = 0; y < 16; y++) {
+		u16 *p = dest + pitch*y;
+
+		for (x = 0; x < 8; x++) {
+			u16 b;
 			u32 c;
 
-			c = mem[bitmap + 128*tile + (16*y1 + x1)/2];
-			if (x1 & 1)
-				c >>= 8;
-			else
-				c &= 0xff;
+			b = *m++;
 
-			c = palette[c];
-
+			c = palette[b & 0xff];
 			if (c == (u32)-1)
-				continue;
+				c = *p;
+			*p++ = c;
 
-			dest[y1*screen->pitch/2 + x1] = c;
+			c = palette[b >> 8];
+			if (c == (u32)-1)
+				c = *p;
+			*p++ = c;
 		}
+	}
 }
 
 static void blit_page_16x16x64(u16 *mem, u32 bitmap, u32 tilemap, u32 *palette)
@@ -89,7 +51,7 @@ static void blit_page_16x16x64(u16 *mem, u32 bitmap, u32 tilemap, u32 *palette)
 	for (y0 = 0; y0 < 15; y0++)
 		for (x0 = 0; x0 < 20; x0++) {
 			u16 tile = mem[tilemap + x0 + 32*y0];
-			u16 *buf = (u16 *)screen->pixels + 8*y0*screen->pitch + 16*x0;
+			u16 *buf = screen + 16*pitch*y0 + 16*x0;
 
 			if (tile == 0)
 				continue;
@@ -110,49 +72,49 @@ static void blit_page_16x16x64(u16 *mem, u32 bitmap, u32 tilemap, u32 *palette)
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1] = c;
+					buf[pitch*y1 + 8*x1] = c;
 
 					c = (b0 >> 4) & 0x3f;
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 1] = c;
+					buf[pitch*y1 + 8*x1 + 1] = c;
 
 					c = ((b0 << 2) & 0x3c) | (b1 >> 14);
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 2] = c;
+					buf[pitch*y1 + 8*x1 + 2] = c;
 
 					c = (b1 >> 8) & 0x3f;
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 3] = c;
+					buf[pitch*y1 + 8*x1 + 3] = c;
 
 					c = (b1 >> 2) & 0x3f;
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 4] = c;
+					buf[pitch*y1 + 8*x1 + 4] = c;
 
 					c = ((b1 << 4) & 0x30) | (b2 >> 12);
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 5] = c;
+					buf[pitch*y1 + 8*x1 + 5] = c;
 
 					c = (b2 >> 6) & 0x3f;
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 6] = c;
+					buf[pitch*y1 + 8*x1 + 6] = c;
 
 					c = b2 & 0x3f;
 					c = palette[c];
 					if (c == (u32)-1)
 						continue;
-					buf[y1*screen->pitch/2 + 8*x1 + 7] = c;
+					buf[pitch*y1 + 8*x1 + 7] = c;
 				}
 		}
 }
@@ -164,7 +126,7 @@ static void blit_page_16x16x256(u16 *mem, u32 bitmap, u32 tilemap, u32 *palette)
 	for (y0 = 0; y0 < 15; y0++)
 		for (x0 = 0; x0 < 20; x0++) {
 			u16 tile = mem[tilemap + x0 + 32*y0];
-			u16 *dest = (u16 *)screen->pixels + 8*y0*screen->pitch + 16*x0;
+			u16 *dest = screen + 16*pitch*y0 + 16*x0;
 
 			if (tile == 0)
 				continue;
@@ -234,12 +196,12 @@ void update_screen(u16 *mem)
 		if (c & 0x8000)
 			c = -1;
 		else
-			c = SDL_MapRGB(screen->format, x58(c >> 10), x58(c >> 5), x58(c));
+			c = SDL_MapRGB(surface->format, x58(c >> 10), x58(c >> 5), x58(c));
 		palette[n] = c;
 	}
 
-	if (SDL_MUSTLOCK(screen))
-		if (SDL_LockSurface(screen) < 0) {
+	if (SDL_MUSTLOCK(surface))
+		if (SDL_LockSurface(surface) < 0) {
 			printf("oh crap.\n");
 			exit(1);
 		}
@@ -247,8 +209,26 @@ void update_screen(u16 *mem)
 	blit_page(mem, 0, 0x40*mem[0x2820], mem[0x2814], mem[0x2812], palette);
 	blit_page(mem, 1, 0x40*mem[0x2821], mem[0x281a], mem[0x2818], palette);
 
-	if (SDL_MUSTLOCK(screen))
-		SDL_UnlockSurface(screen);
+	if (SDL_MUSTLOCK(surface))
+		SDL_UnlockSurface(surface);
 
-	SDL_UpdateRect(screen, 0, 0, 320, 240);
+	SDL_UpdateRect(surface, 0, 0, 320, 240);
+}
+
+void sdl_init(void)
+{
+	if (SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0) {
+		fprintf(stderr, "Unable to init SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+	atexit(SDL_Quit);
+
+	surface = SDL_SetVideoMode(320, 240, 16, SDL_SWSURFACE);
+	if (!surface) {
+		fprintf(stderr, "Unable to set 320x240 video: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	screen = surface->pixels;
+	pitch = surface->pitch / 2;
 }
