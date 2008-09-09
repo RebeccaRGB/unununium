d5bc15d4a1358eac26ce96db67ce0fa2bc721d00
Segher Boessenkool <segher@kernel.crashing.org>
2008-09-09 05:05:35 +0200

    Add copyright and license statement to sdl.[ch]

--- sdl.c
+++ sdl.c
@@ -1,3 +1,7 @@
+// Copyright 2008  Segher Boessenkool  <segher@kernel.crashing.org>
+// Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
 #include <stdlib.h>
 
 #include "types.h"
--- sdl.h
+++ sdl.h
@@ -1,3 +1,7 @@
+// Copyright 2008  Segher Boessenkool  <segher@kernel.crashing.org>
+// Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
 // Cannot name this "_SDL_H", the stupid SDL headers claim that for itself.
 #ifndef _MY_SDL_H
 #define _MY_SDL_H
