fed240e80a4adb87a6d5c3cf943f59ab62fc3f58
Segher Boessenkool <segher@kernel.crashing.org>
2008-10-01 16:16:52 +0200

    Implement a SEEPROM on IIC.

--- io.c
+++ io.c
@@ -14,6 +14,23 @@
 static int uart_in_count = 0;
 
 
+static u8 seeprom[0x400];
+
+static void do_i2c(void)
+{
+	u32 addr;
+
+	addr = (mem[0x3d5b] & 0x06) << 7;
+	addr |= mem[0x3d5c] & 0xff;
+
+	if (mem[0x3d58] & 0x40)
+		mem[0x3d5e] = seeprom[addr];
+	else
+		seeprom[addr] = mem[0x3d5d];
+
+	mem[0x3d59] |= 1;
+}
+
 void io_store(u16 val, u32 addr)
 {
 	switch (addr) {
@@ -58,23 +75,19 @@ void io_store(u16 val, u32 addr)
 		break;
 
 	case 0x3d58:		// IIC command
-		printf("IO STORE %04x to %04x\n", val, addr);
+		do_i2c();
 		break;
 
 	case 0x3d59:		// IIC status/ack
-		printf("IO STORE %04x to %04x\n", val, addr);
-		break;
+		mem[addr] &= ~val;
+		return;
 
 	case 0x3d5a:		// IIC access mode?  always 80, "combined mode"
 		printf("IO STORE %04x to %04x\n", val, addr);
 		break;
 
 	case 0x3d5b:		// IIC device address
-		printf("IO STORE %04x to %04x\n", val, addr);
-		break;
-
 	case 0x3d5c:		// IIC subadr
-		printf("IO STORE %04x to %04x\n", val, addr);
 		break;
 
 	case 0x3d5d:		// IIC data out
@@ -117,6 +130,12 @@ u16 io_load(u32 addr)
 		val = controller_input[uart_in_count];
 		uart_in_count = (uart_in_count + 1) % 8;
 		return val;
+
+	case 0x3d59:			// IIC status
+		break;
+
+	case 0x3d5e:			// IIC data in
+		break;
 
 	default:
 		printf("IO LOAD %04x from %04x\n", val, addr);
