5b76a1130e61066cdeda6e24ddce4e819d71f982
Segher Boessenkool <segher@kernel.crashing.org>
2009-03-09 00:11:41 +0100

    audio: Create the SPU 6.75MHz stream, downsample it to ~44.1kHz

--- audio.c
+++ audio.c
@@ -50,15 +50,50 @@ u16 audio_load(u32 addr)
 	return val;
 }
 
+static u32 get_channel_bit(u32 ch, u32 reg)
+{
+	reg += 0x3400;
+	if (ch >= 16) {
+		reg += 0x0100;
+		ch -= 16;
+	}
+	return (mem[reg] >> ch) & 1;
+}
+
 #define FREQ 440
+static u16 next_channel_sample(u32 ch)
+{
+//	if (ch)
+//		return 0x8000;
+	static u32 xxx[24] = {0};
+	u16 sample = 0x8000 + (int)(20000*sin(2*M_PI*xxx[ch]*FREQ/281250));
+	xxx[ch]++;
+	if (xxx[ch] == 281250)
+		xxx[ch] = 0;
+	return sample;
+}
+
+static u16 next_sample(void)
+{
+	static u32 ch = 0;
+	u16 sample;
+	if (get_channel_bit(ch, 0))
+		sample = next_channel_sample(ch);
+	else
+		sample = 0x8000;
+	ch++;
+	if (ch == 24)
+		ch = 0;
+	return sample;
+}
+
 static s16 next_host_sample(void)
 {
-	static u32 xxx = 0;
-	s16 sample = 4000*sin(2*M_PI*xxx*FREQ/44100);
-	xxx++;
-	if (xxx == 44100)
-		xxx = 0;
-	return sample;
+	u32 acc = 0;
+	u32 i;
+	for (i = 0; i < 153; i++)
+		acc += next_sample();
+	return (acc / 153) - 0x8000;
 }
 
 void audio_render(s16 *data, u32 n)
