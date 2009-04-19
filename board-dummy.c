5b6cebeffa4770d7725e84c5b9e95ae935f7bd39
Segher Boessenkool <segher@kernel.crashing.org>
2009-04-20 01:40:05 +0200

    Whoops, forgot to commit board-dummy.c

---
+++ board-dummy.c
@@ -0,0 +1,22 @@
+// Copyright 2008,2009  Segher Boessenkool  <segher@kernel.crashing.org>
+// Licensed under the terms of the GNU GPL, version 2
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+#include <stdio.h>
+
+#include "types.h"
+#include "emu.h"
+#include "platform.h"
+
+#include "board.h"
+
+
+static u16 gpio(u32 n, u16 what, u16 push, u16 pull)
+{
+	return what;
+}
+
+struct board board_dummy = {
+//	.init = init,
+	.gpio = gpio
+};
